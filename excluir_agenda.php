02fe208febe93c6d68713f54bbfb07df836c9307
SamuelAlvesNavarro <125135167+SamuelAlvesNavarro@users.noreply.github.com>
2023-06-01 16:50:28 -0300

    Update excluir_agenda.php

---
+++ excluir_agenda.php
@@ -0,0 +1,14 @@
+<?php
+    require 'conexao.php';
+    $id_agenda = $_GET['id_agenda'];
+    $sql = 'DELETE FROM agenda WHERE id_agenda='.$id_agenda;
+    echo "<h1> Exclusão de Agenda </h1>";
+	$result = mysqli_query($con, $sql);
+	if($result){
+		header("Location:listar_agendas.php");
+  }
+	else{
+		echo "Erro ao tentar excluir usuário: ".mysqli_error($con) . "<br>";
+    echo "<a href='listar_agendas.php'>Voltar</a>";
+  }
+?>
