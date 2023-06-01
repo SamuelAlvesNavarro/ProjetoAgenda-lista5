e221c4a31f8fcf61b23b45fc90c119202ad8aefc
SamuelAlvesNavarro <125135167+SamuelAlvesNavarro@users.noreply.github.com>
2023-06-01 16:56:04 -0300

    Update alterar_agenda.exe.php

--- alterar_agenda.exe.php
+++ alterar_agenda.exe.php
@@ -1,1 +1,26 @@
+<?php
+    require 'conexao.php';
+    $id_agenda = $_POST['id_agenda'];
+    $nome = $_POST['nome'];
+    $apelido = $_POST['apelido'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
+    $endereco = $_POST['endereco'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $celular = $_POST['celular'];
 
+
+
+    $sql = "UPDATE agenda SET nome='".$nome."', email='".$email."', telefone='".$tel."', apelido='".$apelido."', endereco='".$endereco."', bairro='".$bairro."', cidade='".$cidade."', estado='".$estado."', celular='".$celular."' WHERE id_agenda=".$id_agenda;
+    $result = mysqli_query($con, $sql);
+
+    if($result){
+        header("Location:listar_agendas.php");
+    } else{ 
+        echo "Erro ao alterar o banco de dados: ".mysqli_error($con)."<br>";
+        echo "<a href='index.php'>Voltar</a>";
+    }
+
+?>
