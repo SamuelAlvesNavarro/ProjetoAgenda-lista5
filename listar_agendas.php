c3d7d43171e1761316364d5c672420039285cfe1
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-06-05 19:20:48 -0300

    at

--- listar_agendas.php
+++ listar_agendas.php
@@ -8,8 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
-<body class="p-5">
-    <div class="text-center mb-3">
+<body class="p-4">
+    <div class="text-center">
         <div class="row align-items-start">
             <h1 align="center-2" class="col">Consulta de agenda</h1>
                 <?php
@@ -23,7 +23,7 @@
                 ?>
             <a href="index.php" class="p-2 col-2"><button type="button" class="btn-close" disabled aria-label="Close"></button></a>
         
-            <table align="center" class="col-3 table table-striped border border-black border-1 p-2">
+            <table align="center" class="col-3 table table-striped border border-black border-1">
                 <thead class="thead-dark">
                     <tr>
                         <th scope="col">Código</th>
@@ -36,6 +36,7 @@
                         <th scope="col">Estado</th>
                         <th scope="col">Cidade</th>
                         <th scope="col">Celular</th>
+                        <th scope="col">Data de Criação</th>
                         <th scope="col">Alterar</th>
                         <th scope="col">Excluir</th>
                     </tr>
@@ -54,6 +55,7 @@
                             echo "<td>".$row['cidade']."</td>";
                             echo "<td>".$row['estado']."</td>";
                             echo "<td>".$row['celular']."</td>";
+                            echo "<td>".$row['dt_cadastro']."</td>";
                             echo "<td><a href='alterar_agendas.php?id_agenda=". $row['id_agenda'] ."'><button class='btn btn-primary'>Mudar</button></a></td>";
                             echo "<td><a href='excluir_agenda.php?id_agenda=". $row['id_agenda'] ."'><button class='btn btn-danger'>Excluir</button></a></td>";
                             echo "</tr>";
